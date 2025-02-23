2ed665c31a64f8c422ba016d59c77eb2aa5773bd
shivchauhan <itsmeshivchauhan@gmail.com>
2025-02-23 17:07:30 +0530

    first commit

---
+++ src/test/java/com/orangeHrm/automation/Main.java
@@ -0,0 +1,8 @@
+package com.orangeHrm.automation;
+
+public class Main {
+    public static void main(String[] args){
+        LoginPage loginPage = new LoginPage();
+        loginPage.validLogin();
+    }
+}
